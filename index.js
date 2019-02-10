d320e6a7cd00cc9c054f91bb9f10e272e2c1cd81
Jamie McGrory <contact@jmcgr.co.uk>
2019-02-10 12:19:15 +0000

    moving index file

---
+++ index.js
@@ -0,0 +1,33 @@
+import Article from './src/components/Article';
+import Field from './src/components/Field';
+import FieldSet from './src/components/FieldSet';
+import Footer from './src/components/Footer';
+import Header from './src/components/Header';
+import Heading from './src/components/Heading';
+import Link from './src/components/Link';
+import List from './src/components/List';
+import ListItem from './src/components/ListItem';
+import Media from './src/components/Media';
+import Nav from './src/components/Nav';
+import Section from './src/components/Section';
+import Table from './src/components/Table';
+import TableCell from './src/components/TableCell';
+import TableRow from './src/components/TableRow';
+
+export {
+    Article,
+    Field,
+    FieldSet,
+    Footer,
+    Header,
+    Heading,
+    Link,
+    List,
+    ListItem,
+    Media,
+    Nav,
+    Section,
+    Table,
+    TableCell,
+    TableRow,
+}
