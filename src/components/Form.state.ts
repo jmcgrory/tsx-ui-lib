6a7ce1d369b62db6971ae0f9935b1974c57bfd43
Jamie McGrory <contact@jmcgr.co.uk>
2019-02-10 11:43:02 +0000

    removing stateful components

--- src/components/Form.state.ts
+++
@@ -1,9 +0,0 @@
-import FieldProps from "./Field.props";
-
-interface FormState {
-    hasValidation: boolean;
-    fields: FieldProps[];
-    notices: any[]; // TODO: Notice Component Props
-}
-
-export default FormState;
