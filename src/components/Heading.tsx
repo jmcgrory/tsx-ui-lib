0ada17a7de40abc51c4d2fd8bedf3f9cac7b47d2
Jamie McGrory <contact@jmcgr.co.uk>
2019-02-10 11:43:23 +0000

    simplifying Heading for ts intrinsicattr issue

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,8 +2,30 @@ import React from 'react';
 import HeadingProps from "./Heading.props";
 
 const Heading = ({ title, level = 1 } : HeadingProps) => {
-    const Tag = `h${level}`;
-    return <Tag className={'heading'}>{ title }</Tag>;
+    const attributes = {
+        className: 'heading',
+        children: title,
+    }
+    switch(level){
+        case 1:
+            return <h1 {...attributes } />;
+            break;
+        case 2:
+            return <h2 {...attributes } />;
+            break;
+        case 3:
+            return <h3 {...attributes } />;
+            break;
+        case 4:
+            return <h4 {...attributes } />;
+            break;
+        case 5:
+            return <h5 {...attributes } />;
+            break;
+        case 6:
+            return <h6 {...attributes } />;
+            break;
+    }
 }
 
 export default Heading;
